5177c9f8be6ade427e42827252acd52303b9b99a
Sedruol <destructor2104992013@gmail.com>
2024-04-03 23:22:26 -0500

    T1 Preparation

---
+++ Assets/T1/_Complete-Game/Scripts/Managers/EnemySpawner.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CompleteProject
+{
+    public class EnemySpawner : MonoBehaviour
+    {
+        //---------
+        //Variables
+        //---------
+                                 // Referencia al Script PlayerHealth
+                                // Los prefabs de los enemigos que se usarán en el spawn
+                               // Los tiempos que habrá entre cada spawn de cada enemigo
+                               // Los transforms de los puntos de spawn de cada enemigo
+
+        //-----
+        //Start
+        //-----
+        void Start()
+        {
+            
+                
+        }
+        //-----------
+        //Ienumerator
+        //-----------
+        
+        
+            
+            // ¿El jugador ya no tiene vida?
+            
+            
+                // Salimos de la función
+                
+            
+            
+            
+        
+    }
+}
